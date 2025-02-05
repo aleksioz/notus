3a11047ffda84dba8d88c7bb47037f9f485dcb16
aleksioz <aleksa.mejl@gmail.com>
2025-02-05 05:04:32 +0100

    Update JSON response headers and encoding for product routes

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
 
-// Enable error reporting
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 // Require composer autoloader
 require __DIR__ . '/vendor/autoload.php';
-
-
 
 $router = new \Bramus\Router\Router();
 $controller = new App\Controller();
 
 // Define header - to pretty print JSON
 $router->before('GET', '/.*', function () {
-    header('Content-Type: application/json');
+    header('Content-Type: application/json; charset=UTF-8');
 });
 
 ////////////// PROVIDER ROUTES //////////////
@@ -23,36 +17,40 @@ $router->get('/products', function () use ($controller) {
     $skip = $_GET['skip'] ?? 0;
     $sortBy = $_GET['sortBy'] ?? 'id';
     $order = $_GET['order'] ?? 'asc'; // Default
-    $products = $controller->listProducts($limit, $skip, $sortBy, $order);
-    echo json_encode($products, JSON_PRETTY_PRINT);
+    $products = $controller->listProducts( $limit, $skip, $sortBy, $order);
+    echo json_encode($products, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
 $router->get('/products/(\d+)', function ($id) use ($controller) {
     $product = $controller->getProduct($id);
-    echo json_encode($product, JSON_PRETTY_PRINT);
+    echo json_encode($product, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_UNICODE);
 });
 
 $router->get('/products/search', function () use ($controller) {
     $query = $_GET['q'] ?? '';
 	$products = $controller->searchProducts($query);
-	echo json_encode($products, JSON_PRETTY_PRINT);
+	echo json_encode($products, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
 ////////////// PARSER ROUTES //////////////
 $router->get('/parsed/products', function () use ($controller) {
-    $products = $controller->listProductsParsed();
-    echo json_encode($products, JSON_PRETTY_PRINT);
+    $limit = $_GET['limit'] ?? 10;
+    $skip = $_GET['skip'] ?? 0;
+    $sortBy = $_GET['sortBy'] ?? 'id';
+    $order = $_GET['order'] ?? 'asc'; // Default
+    $products = $controller->listProductsParsed( $limit, $skip, $sortBy, $order );
+    echo json_encode($products, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
 $router->get('/parsed/products/(\d+)', function ($id) use ($controller) {
     $product = $controller->getProductParsed($id);
-    echo json_encode($product, JSON_PRETTY_PRINT);
+    echo json_encode($product, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
 $router->get('/parsed/products/search', function () use ($controller) {
     $query = $_GET['q'] ?? '';
     $products = $controller->searchProductsParsed($query);
-    echo json_encode($products, JSON_PRETTY_PRINT);
+    echo json_encode($products, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
 
