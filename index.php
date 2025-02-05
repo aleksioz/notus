04b9a40295046260755e039a9aa9f90dc186298b
aleksioz <aleksa.mejl@gmail.com>
2025-02-05 02:57:45 +0100

    Add provider routes

--- index.php
+++ index.php
@@ -1,15 +1,46 @@
 <?php
 
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Require composer autoloader
 require __DIR__ . '/vendor/autoload.php';
 
-// Create Router instance
-$router = new \Bramus\Router\Router();
 
-// Define routes
-$router->get('/', function () {
-    echo 'Test';
+
+$router = new \Bramus\Router\Router();
+$controller = new App\Controller();
+
+// Define header - to pretty print JSON
+$router->before('GET', '/.*', function () {
+    header('Content-Type: application/json');
 });
+
+////////////// PROVIDER ROUTES //////////////
+$router->get('/products', function () use ($controller) {
+    $products = $controller->listProducts();
+    echo json_encode($products, JSON_PRETTY_PRINT);
+});
+
+$router->get('/products/(\d+)', function ($id) use ($controller) {
+    $product = $controller->getProduct($id);
+    echo json_encode($product, JSON_PRETTY_PRINT);
+});
+
+$router->get('/products/search/(\w+)', function ($query) use ($controller) {
+	$products = $controller->searchProducts($query);
+	echo json_encode($products, JSON_PRETTY_PRINT);
+});
+
+////////////// PARSER ROUTES //////////////
+
+
+// Home route
+$router->get('/', function () {
+    echo json_encode(['message' => 'Welcome to the API']);
+});
+
 
 // Run it!
 $router->run();
